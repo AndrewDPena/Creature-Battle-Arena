aedd1dff4f67817d2a599e6a615530154b6e83da
BahamutAPs <33669440+BahamutAPs@users.noreply.github.com>
2021-02-23 06:47:19 -0700

    Added prefabs; may not be created correctly.

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -7,22 +7,22 @@ using UnityEngine.UI;
 public class HealthBar : MonoBehaviour
 {
 
-    public Slider slider;
-    public Gradient gradient;
-    public Image fill;
+    public Slider Slider;
+    public Gradient Gradient;
+    public Image Fill;
     
     public void SetMaxHealth(int health)
         {
-            slider.maxValue = health;
-            slider.value = health;
+            Slider.maxValue = health;
+            Slider.value = health;
 
-            fill.color = gradient.Evaluate(1f);
+            Fill.color = Gradient.Evaluate(1f);
         }
     
     public void SetHealth(int health)
     {
-        slider.value = health;
-        fill.color = gradient.Evaluate(slider.normalizedValue);
+        Slider.value = health;
+        Fill.color = Gradient.Evaluate(Slider.normalizedValue);
     }
 
     
--- Assets/Scripts/PlayerCreature.cs
+++ Assets/Scripts/PlayerCreature.cs
@@ -1,23 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 
 public class PlayerCreature : MonoBehaviour
 {
     public int MaxHealth = 69;
     public int CurrentHealth;
-
+    [SerializeReference]
     public HealthBar HealthBar;
-
     
-    // Start is called before the first frame update
     private void Start()
     {
         CurrentHealth = MaxHealth;
         HealthBar.SetMaxHealth(MaxHealth);
     }
 
-    // Update is called once per frame
     private void Update()
     {
         
