28eb58548cfd49011ad506afb8b3fe087d864564
Andrew Pena <masterbahamut07@gmail.com>
2021-03-31 02:38:10 -0600

    Changed fire breath to use event system, modified prefab to allow for it

--- Assets/Scripts/AttackList/FireBreath.cs
+++ Assets/Scripts/AttackList/FireBreath.cs
@@ -7,18 +7,19 @@ namespace AttackList
 
     public class FireBreath : MonoBehaviour
     {
-        [SerializeField] 
-        private Collider2D _collider;
-        // Start is called before the first frame update
+        private int _battlePower;
+        private int _damagePerUpdate;
+            
         void Start()
         {
-            
+            _battlePower = 10;
+            _damagePerUpdate = 1;
         }
     
-        // Update is called once per frame
         void Update()
         {
-            var results = new Collider2D[10];
+            // This is the godawful damage per frame firebreath
+            /*var results = new Collider2D[10];
             if (Physics2D.OverlapCollider(_collider, new ContactFilter2D(), results) > 0)
             {
                 foreach (var collider in results)
@@ -29,6 +30,17 @@ namespace AttackList
                         creature.TakeDamage(1);
                     }
                 }
+            }*/
+        }
+
+        private void OnTriggerStay2D(Collider2D other)
+        {
+            Debug.Log("Events are working.");
+            var c = other.GetComponentInParent<Creature>();
+            if (c != null & _battlePower > 0)
+            {
+                c.TakeDamage(_damagePerUpdate);
+                _battlePower -= _damagePerUpdate;
             }
         }
     }
