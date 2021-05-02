9972c3ebc32fd8d8a26d1c28dd06497a6a3ca986
Andrew Pena <masterbahamut07@gmail.com>
2021-05-02 03:24:05 -0600

    Created Creaturedex for better organization of entire creature list, pocket setup now saves values, still needs to be passed on

---
+++ Assets/Scripts/Creaturedex.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Creaturedex : MonoBehaviour
+{
+    [SerializeField] private CreatureBase[] _creatureDex;
+
+    public CreatureBase GetCreatureByDexNumber(int dexNo)
+    {
+
+        return dexNo > _creatureDex.Length ? _creatureDex[0] : _creatureDex[dexNo - 1];
+    }
+}
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,17 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    [SerializeField] private GameObject _pocketMenu;
-    [SerializeField] private GameObject _mainMenu;
-    
-    public void LoadPocketSetup()
-    {
-        
-    }
+    //[SerializeField] private GameObject _pocketMenu;
+    //[SerializeField] private GameObject _mainMenu;
+    [SerializeField] private Creaturedex _creatureDex;
+    [SerializeField] private Dropdown[] _playerSlots;
+    [SerializeField] private Dropdown[] _npcSlots;
+    [SerializeField] private List<CreatureBase> _playerCreatures;
+    [SerializeField] private List<CreatureBase> _npcCreatures;
     
     public void LoadBattleScene()
     {
@@ -20,6 +20,25 @@ public class MainMenu : MonoBehaviour
 
     public void SavePocket()
     {
-        Debug.Log("Success");
+        _playerCreatures = new List<CreatureBase>();
+        _npcCreatures = new List<CreatureBase>();
+        _playerSlots[0].value += 1; // Accounts for lack of "None" option
+        _npcSlots[0].value += 1;
+        foreach (var slot in _playerSlots)
+        {
+            if (slot.value == 0)
+            {
+                continue;
+            }
+            _playerCreatures.Add(_creatureDex.GetCreatureByDexNumber(slot.value));
+        }
+        foreach (var slot in _npcSlots)
+        {
+            if (slot.value == 0)
+            {
+                continue;
+            }
+            _npcCreatures.Add(_creatureDex.GetCreatureByDexNumber(slot.value));
+        }
     }
 }
